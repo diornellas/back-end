475bb9e6ec7da2e84260a844eb00ea739da6a62a
diornellas <diornellas999@gmail.com>
2022-05-31 18:22:48 -0300

    Nova atualizacao

--- python/aula012/desafios.py
+++ python/aula012/desafios.py
@@ -1,11 +1,10 @@
 # NUMEROS COM FLAG
-c = 1
 tot = s = 0
 while True:
    num = int(input('Digite um numero:[999 parar programa!]'))
    if num == 999:
       break
-   tot += c
+   tot += 1
    s += num
 print(f'Total de Numeros digitados {tot} a soma deles é {s}')
 #-----------------------------------------------------------------------
@@ -28,53 +27,50 @@ print('Volte sempre!!!')
 # JOGO DO PAR OU IMPAR
 from random import randint
 num = soma = v = 0
-opcao = r = escolheu = ''
 print('=-'*25)
 print('VAMOS JOGAR PAR OU IMPAR')
 print('=-'*25)
 print('--'*25)
 while True:
-   pc = randint(0,10)
    num = int(input('Diga o valor: '))
-   opcao = str(input('Par ou Impar? [PAR/IMPAR] ')).upper()
+   pc = randint(0,10)
    soma = pc + num
-   if soma % 2 == 0:
-      r = 'PAR'
-   elif soma % 2 == 1:
-         r = 'IMPAR'
-   if num and pc % 2 == 0:
-      escolheu = 'PAR'
-   elif num and pc % 2 == 1:
-         escolheu = 'IMPAR'
-   if opcao == r:
-      print('--'*25)
-      v += 1
-      print(f'Você jogou {num} e o computador {pc}. Total {soma} DEU {r}')
-      print('--'*25)
-      print('Você VENCEU!')
-      print('Vamos Jogar Novamente...')
-      print('~~'*25)
-   elif escolheu != r:
-      print('--'*25)
-      print(f'Você jogou {num} e o computador {pc}. Total {soma} DEU {r}')
-      print('--'*25)
-      print('Você PERDEU!')
-      print('~~'*25)
-      break
+   opcao = ' '
+   while opcao not in 'PI':
+      opcao = str(input('Par ou Impar? [PAR/IMPAR] ')).strip().upper()[0]
+   print(f'Você jogou {num} e o computador {pc}. Total {soma}', end=' ')
+   print('Deu PAR' if soma % 2 == 0 else 'Deu IMPAR')
+   if opcao == 'P':
+      if soma % 2 ==0:
+         print('Você venceu!')
+         v += 1
+      else:
+         print('Você perdeu!')
+         break
+   elif opcao == 'I':
+      if soma % 2 ==1:
+         print('Você venceu!')
+         v += 1
+      else:
+         print('Você Perdeu!')
+         break
+   print('Vamos Jogar Novamente...')
 print(f'GAME OVER! Você venceu {v} vezes.')
 #-----------------------------------------------------------------------------------
 # ANALISE DE DADOS DO GRUPO
 idade = tot = masc = novinha = 0
-sexo = resp = ''
 while True:
    print('--'*25)
    print('CADASTRO UMA PESSOA')
    print('--'*25)
+   sexo = resp = ' '
    idade = int(input('Idade: '))
-   sexo = str(input('Sexo: ')).upper()
+   while sexo not in 'MF':
+      sexo = str(input('Sexo:[M/F]')).strip().upper()
    print('--'*25)
-   resp = str(input('Quer continuar?[S/N] ')).upper()
-   if idade > 18:
+   while resp not in 'SN':
+      resp = str(input('Quer continuar?[S/N] ')).strip().upper()
+   if idade >= 18:
       tot += 1
    if sexo == 'M':
       masc += 1
@@ -88,20 +84,57 @@ print(f'Ao todo temos {masc} homens cadastrados!')
 print(f'E temos {novinha} mulheres com menos de 20 anos!')
 #-------------------------------------------------------------------------------------
 # ESTATÍSTICAS EM PRODUTOS
-total = acao = maisMil = 0
+barato = ''
+total = preco = maisMil = menor = c = 0
 while True:
    print('=~'*25)
    produto = str(input('Nome do produto: '))
    preco = float(input('Qual o preço do produto: R$'))
+   c += 1
    print('~='*25)
    acao = str(input('Quer continuar?[S/N] ')).upper()
    total += preco
    if preco > 1000:
       maisMil += 1
+   if c == 1:
+      menor = preco
+      barato = produto
+   else:
+      if preco < menor:
+         menor = preco
+         barato = produto
    if acao == 'N':
       break
 print('~~~~~~~~~~FIM~~~~~~~~~~')
 print(f'Total da compra: R${total:.2f}')
-print(f'Total {maisMil} produtos mais que R$1000,00')
+print(f'Total {maisMil} produtos que custa mais que R$1000,00')
+print(f'O produto mais barato foi {barato} custa R${menor:.2f}')
 #-------------------------------------------------------------------------------------
 #  SIMULADOR DE CAIXA ELETRONICO
+banco = 'BANCO MD'
+print('='*20)
+print('BANCO MD')
+print('='*20)
+valor = int(input('Que Valor Quer Sacar? R$'))
+total = valor
+cedula = 50
+totced = 0
+while True:
+   if total >= cedula:
+      total -= cedula
+      totced += 1
+   else:
+      if totced > 0:
+         print(f'Total {totced} cédulas de R${cedula}')
+      if cedula == 50:
+         cedula = 20
+         totced = 0
+      elif cedula == 20:
+         cedula = 10
+         totced = 0
+      elif cedula == 10:
+         cedula = 1
+         totced = 0
+      if total == 0:
+         break
+print(f'Volte Sempre ao {banco}')
